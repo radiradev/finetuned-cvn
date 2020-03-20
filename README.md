041b9a0a453fb642435945b9ed01b7f811e7f7a2
Saul Alonso Monsalve <saul.alonso.monsalve@cern.ch>
2020-03-20 16:32:56 +0000

    Update README.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# CVN paper
+# DUNE CVN
 
-Code for CVN paper.
+DUNE CVN [code](modules/dune_cvn.py).
+
+# Usage
+
+```
+python main.py
+```
+
+Optional arguments: 
+
+| Parameter                 | Default       | Description   |	
+| :------------------------ |:-------------:| :-------------|
+| --batch_size 	       |	10          |Batch size
+| --model         |        'saved_model/model.json'          |JSON model
+| --weights         |        'saved_model/weights.h5'          |HDF5 pre-trained model weights
+| --dataset         |        'dataset'          |Dataset path
+| --partition         |        'dataset/partition.p'          |Pickled partition
+| --shuffle         |        False          |Shuffle partition
+| --print_model         |        False          |Print model summary
+| --output_file         |        'output/results.txt'          |Output file
--- modules/opts.py
+++ modules/opts.py
@@ -12,13 +12,13 @@ def get_args():
     parser = ArgumentParser(description='DUNE CVN')
     arg = parser.add_argument
     arg('--batch_size', type=int, default=10, help='batch size')
-    arg('--model',  type=str, default='saved_model/model.json', help='JSON model path')
+    arg('--model',  type=str, default='saved_model/model.json', help='JSON model')
     arg('--weights',  type=str, default='saved_model/weights.h5', help='HDF5 pretrained model weights')
     arg('--dataset',type=str, default='dataset', help='Dataset path')
-    arg('--partition',type=str, default='dataset/partition.p', help='Pickle partition path')
+    arg('--partition',type=str, default='dataset/partition.p', help='Pickled partition')
     arg('--shuffle',type=bool, default=False, help='Shuffle partition')
     arg('--print_model',type=bool, default=False, help='Print model summary')
-    arg('--output_file',type=str, default='output/results.txt', help='Output file path')
+    arg('--output_file',type=str, default='output/results.txt', help='Output file')
 
     args = parser.parse_args()
     return args
